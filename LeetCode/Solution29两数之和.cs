c7bcf2962c2f9bb7fa14d00365924adefd0750bc
amos.cao <amos.cao@avepoint.com>
2023-04-12 09:56:28 +0800

    两数之和

---
+++ LeetCode/Solution29两数之和.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode;
+
+/// <summary>
+/// 两数之和
+/// </summary>
+public class Solution29两数之和
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        //暴力枚举
+        // int n = nums.Length;
+        // for (int i = 0; i < n; i++)
+        // {
+        //     for (int j = i + 1; j < n; j++)
+        //     {
+        //         if (nums[i] + nums[j] == target)
+        //         {
+        //             return new int[] { i, j };
+        //         }
+        //     }
+        // }
+        //
+        // return new int[0];
+
+        //哈希表 将寻找target-nums[i]这个循环降为o(1)
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dictionary.ContainsKey(target - nums[i]))
+            {
+                return new int[] { dictionary[target - nums[i]],i };
+            }
+            dictionary.TryAdd(nums[i],i);
+        }
+
+        return new int[0];
+    }
+}
