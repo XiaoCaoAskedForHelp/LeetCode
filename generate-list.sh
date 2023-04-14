640662a2ce8e8ac4a168b90fc25d9546d2ff33cd
amos.cao <amos.cao@avepoint.com>
2023-04-14 16:25:36 +0800

    完善自动更新readme脚本

--- generate-list.sh
+++ generate-list.sh
@@ -6,12 +6,23 @@
 # sed 命令的作用是将匹配 '^./LeetCode' 的子字符串（即以 ./LeetCode 开头的子字符串）替换为空字符串。具体来说，'|^./LeetCode|' 使用 | 替换了 s 操作符两侧的 /，而这是为了避免与 s 中使用的 / 冲突。
 # 因此，命令的整体作用是将 ./LeetCode 替换为空串，输出结果为 "二进制"。
 directories=$(find ./LeetCode -maxdepth 1 -type d | grep -vE "^./LeetCode$|bin|obj|.idea" | sed 's|^./LeetCode/||')
+basePath="https://github.com/XiaoCaoAskedForHelp/LeetCode/tree/main/LeetCode/"
+fileBasePath="https://github.com/XiaoCaoAskedForHelp/LeetCode/blob/main/LeetCode/"
 
 # generate table of contents with directories and paths
 toc=""
 for i in $directories; do
-  path=""
-  toc="$toc\n- [\`$i\`]($path)"
+  echo $i
+  dirPath=${basePath}${i}
+  toc="$toc\n- [\`$i\`]($dirPath)"
+  dir="./LeetCode/${i}"
+  echo $dir
+  files=$(find ${dir} -maxdepth 1 -type f | sed "s|^${dir}/||")
+  for j in $files; do
+    echo $j
+    filePath=${fileBasePath}${i}/${j}
+    toc="$toc\n  - [\`$j\`]($filePath)"
+  done
 done
 
 # update README with table of contents
