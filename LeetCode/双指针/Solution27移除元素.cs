7e8557100a07f53d7f1a701a5dc28f777b96e5e0
amos.cao <amos.cao@avepoint.com>
2023-04-20 09:46:21 +0800

    Solution35搜索插入位置 Solution27移除元素

---
+++ LeetCode/双指针/Solution27移除元素.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.双指针;
+
+public class Solution27移除元素
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        // 简单双指针
+        int init = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[init++] = nums[i];
+            }
+        }
+
+        return init;
+    }
+    
+    // 双指针优化
+    // 前提：「元素的顺序可以改变」
+    // 实现方面，我们依然使用双指针，两个指针初始时分别位于数组的首尾，向中间移动遍历该序列。
+    // 这样的方法两个指针在最坏的情况下合起来只遍历了数组一次。与方法一不同的是，方法二避免了需要保留的元素的重复赋值操作。
+    public int RemoveElement1(int[] nums, int val)
+    {
+        int left = 0, right = nums.Length - 1;
+        while (left <= right)
+        {
+            if (nums[left] == val)
+            {
+                nums[left] = nums[right];
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+
+        return left;
+    }
+}
---
+++ LeetCode/双指针/Solution35搜索插入位置.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.双指针;
+
+public class Solution35搜索插入位置
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length - 1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+    
+    public int searchInsert2(int[] nums, int target) {
+        int n = nums.Length;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = ((right - left) >> 1) + left;
+            if (target <= nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
