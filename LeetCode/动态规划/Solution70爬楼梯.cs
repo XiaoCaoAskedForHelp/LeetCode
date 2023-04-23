d598c0a59bff9165b7635abd8ba53b07669057f4
cqd <amos.cao@outlook.com>
2023-04-23 14:15:35 +0800

    Solution70爬楼梯 Solution69x的平方根

---
+++ LeetCode/动态规划/Solution70爬楼梯.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.动态规划;
+
+public class Solution70爬楼梯
+{
+    // 动态规划f(x)=f(x−1)+f(x−2)
+    public int ClimbStairs(int n)
+    {
+        int p = 0, q = 0, r = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+
+        return r;
+    }
+}
---
+++ LeetCode/数学/Solution69x的平方根.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode;
+
+public class Solution69x的平方根
+{
+    public int MySqrt(int x) {
+        for (long i = 0; i <= x/2 + 2; i++)
+        {
+            if (i * i > x)
+            {
+                return (int)(i - 1);
+            }
+        }
+
+        return 0;
+    }
+    
+    //写成幂的形式,再使用自然对数 e 进行换底
+    public int mySqrt2(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        int ans = (int) Math.Exp(0.5 * Math.Log(x));
+        return (long) (ans + 1) * (ans + 1) <= x ? ans + 1 : ans;
+    }
+    
+    //二分查找
+    public int mySqrt3(int x)
+    {
+        int l = 0, r = x/2 + 1, ans = -1;
+        while (l <= r)
+        {
+            int mid = (l + r) / 2;
+            if ((long)mid * mid <= x)
+            {
+                ans = mid;
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
