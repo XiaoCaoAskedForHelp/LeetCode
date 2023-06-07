072d2a71269bc6c574b7bd00c72c935baa46998b
cqd <amos.cao@outlook.com>
2023-06-07 14:08:35 +0800

    Solution454四数相加II

---
+++ LeetCode/哈希表/Solution454四数相加II.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.哈希表;
+
+public class Solution454四数相加II
+{
+    // 分组 + 哈希表
+    public int FourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        foreach (var i in nums1)
+        {
+            foreach (var j in nums2)
+            {
+                int sum = i + j;
+                if (dict.ContainsKey(sum))
+                {
+                    dict[sum]++;
+                }
+                else
+                {
+                    dict.Add(sum, 1);
+                }
+            }
+        }
+
+        int res = 0;
+        foreach (var a in nums3)
+        {
+            foreach (var b in nums4)
+            {
+                int sum = a + b;
+                if (dict.TryGetValue(-sum, out var result))
+                {
+                    res += result;
+                }
+            }
+        }
+
+        return res;
+    }
+}
